263581cf53e1de5887bef5d61506d82e93a2e045
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-10-09 20:43:26 +0530

    Create Number Pattern 2.java

---
+++ A3. Conditionals and Loops/A3. Patterns/Assignments/Number Pattern 2.java
@@ -0,0 +1,50 @@
+Number Pattern 2
+
+Print the following pattern
+Pattern for N = 4
+       1
+      23
+     345
+    4567
+    
+Input Format :
+N (Total no. of rows)
+Output Format :
+Pattern in N lines
+
+Sample Input :
+5
+Sample Output :
+          1
+         23
+        345
+       4567
+      56789
+ 
+ Code: 
+ 
+import java.util.*;
+public class Solution
+{
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+	    int n = sc.nextInt();
+	    int k = 0;
+		for (int i = 1; i <= n; i++)
+		{
+            for(int j = 1; j <= n-i; j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    k = i;
+		    for(int j = 1; j <= i; j++)
+		    {
+		        
+		        System.out.print(k);
+		        k++;
+		    }
+		    System.out.println();
+ 		}
+       
+	}
+}
