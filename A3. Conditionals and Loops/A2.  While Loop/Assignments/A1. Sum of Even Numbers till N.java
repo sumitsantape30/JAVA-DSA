8b1d270ed4185cae02fed5a7bbbf953a27b902ae
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-10-09 20:37:00 +0530

    Create A1. Sum of Even Numbers till N.java

---
+++ A3. Conditionals and Loops/A2.  While Loop/Assignments/A1. Sum of Even Numbers till N.java
@@ -0,0 +1,34 @@
+Sum of Even Numbers till N
+
+Given a number N, print sum of all even numbers from 1 to N.
+
+Input Format :
+Integer N
+Output Format :
+Required Sum 
+
+Sample Input 1 :
+ 6
+Sample Output 1 :
+12
+
+
+import java.util.*;
+public class Solution
+{
+	public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+       int s = 0;
+       for ( int i = 1; i <= n; i++)
+       {
+           if( i % 2 == 0)
+           {
+               s += i;
+           }
+       }
+       System.out.println(s);
+    }
+
+}
