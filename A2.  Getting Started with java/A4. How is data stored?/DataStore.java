dd8accb2ba38819a1fdd645bf412a1eb4ba7c2f8
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-05 23:45:24 +0530

    Update DataStore.java

--- A2.  Getting Started with java/A4. How is data stored?/DataStore.java
+++ A2.  Getting Started with java/A4. How is data stored?/DataStore.java
@@ -1,25 +1,28 @@
+// Data which is in integer form is stored in binary form but for characters binary form is not possible
+// But every character has a integer value= ASCII Value
+// Characters are getting converted into ASCII Values then they are stored.
 
 public class DataType {
 
 	public static void main(String[] args) {
 		char c = 'a';
-		System.out.println(c + 3);
+		System.out.println(c + 3); // Output : 100
 		
 		int i = c + 10;
-		System.out.println(i);
+		System.out.println(i); // Output : 107
 		
 		i = c;
-//		char abc = i;
+//		char abc = i; // You are trying to copy 4bytes into 2bytes so it will give a error
 		
 		long l = i;
-		//i = l;
+		//i = l; // You are trying to copy 8bytes into 2bytes so it will give a error
 		
 		double d = i;
-		//i = d;
+		//i = d; // You are trying to copy 8bytes into 2bytes so it will give a error
 		
 		float f = 1.23f;
-		//f = d;
-		d = f;
+		//f = d; // You are trying to copy 8bytes into 4bytes so it will give a error 
+		d = f; // 8byte mai 4byte store kr skte hai
 		
 		int j = 100;
 		
