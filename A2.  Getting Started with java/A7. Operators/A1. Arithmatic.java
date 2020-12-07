632ba2a72f354573028d37d06559699820873e75
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-07 15:45:30 +0530

    Update A1. Arithmatic.java

--- A2.  Getting Started with java/A7. Operators/A1. Arithmatic.java
+++ A2.  Getting Started with java/A7. Operators/A1. Arithmatic.java
@@ -5,7 +5,7 @@ public class FahToCel {
 	public static void main(String[] args) {
 //		Scanner s = new Scanner(System.in);
 //		int f = s.nextInt();
-//		//int cel = (5/9)*(f - 32); // this is the wrong way
+//		//int cel = (5/9)*(f - 32); 
 //		//int cel = (5 * (f - 32))/9;
 //		
 //		int cel = (int)((5.0/9)*(f - 32));
