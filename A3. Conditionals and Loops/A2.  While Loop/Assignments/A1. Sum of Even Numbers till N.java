476f1134a978d7baeeac392907c6b42e324bd12b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-09 00:50:56 +0530

    Update A1. Sum of Even Numbers till N.java

--- A3. Conditionals and Loops/A2.  While Loop/Assignments/A1. Sum of Even Numbers till N.java
+++ A3. Conditionals and Loops/A2.  While Loop/Assignments/A1. Sum of Even Numbers till N.java
@@ -20,16 +20,18 @@ public class Solution
 {
 	public static void main(String args[])
     {
-        Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-       int s = 0;
-       for ( int i = 1; i <= n; i++)
-       {
-           if( i % 2 == 0)
-           {
-               s += i;
-           }
-       }
-       System.out.println(s);
+                Scanner s= new Scanner(System.in);
+		
+		int n= s.nextInt();
+		int sum=0;
+		int i=1;
+		while(i <= n) {
+			
+			if( i % 2 == 0) {
+             sum= sum + i;
+			}
+			i++;
+		}
+		System.out.println(sum);
     }
 }
