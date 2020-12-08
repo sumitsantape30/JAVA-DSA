fb413b722271507bf3c823cbc8024a0fbe6d63e4
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-08 22:55:09 +0530

    Update A3. IntToString.java

--- A3. Conditionals and Loops/A1. Conditionals/A3. IntToString.java
+++ A3. Conditionals and Loops/A1. Conditionals/A3. IntToString.java
@@ -7,14 +7,21 @@ public class IntToString {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int a = s.nextInt();
-//		if (a == 1) {
-//			System.out.println("One");
-//		} else if (a == 2) {
-//			System.out.println("Two");
-//		} else if (a == 3) {
-//			System.out.println("Three");
-//		}
 		
+//Approach 1 ---------------------------------------------
+                if( n == 1) {
+			System.out.println("One");
+		}else if( n == 2) {
+			System.out.println("Two");
+		}else if( n == 3) {
+			System.out.println("Three");
+		}else {
+			System.out.println("Invalid Input");
+		}
+// Input : 3
+// Output : Three
+		
+//Approach 2 --------------------------------------------
 		if (a == 1) {
 			System.out.println("One");
 		}
@@ -26,6 +33,8 @@ public class IntToString {
 		if (a == 3) {
 			System.out.println("Three");
 		}
+// Input :  1
+// Output : One
 	}
 
 }
