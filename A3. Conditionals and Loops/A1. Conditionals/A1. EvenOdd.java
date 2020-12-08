539f20a844f2db28240bc6ba54668e5d05dbe0f0
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-08 21:54:20 +0530

    Update A1. EvenOdd.java

--- A3. Conditionals and Loops/A1. Conditionals/A1. EvenOdd.java
+++ A3. Conditionals and Loops/A1. Conditionals/A1. EvenOdd.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class EvenOdd {
 
 	public static void main(String[] args) {
+		
+		// Approach 1
 		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		int rem = n % 2;
-		if (rem == 0) {
+		int n=s.nextInt();
+		int rem= n%2;
+		
+		if(rem==0){
 			System.out.println("Even");
-			return;
-		} 
-		System.out.println("Odd");
+		}else{
+			System.out.println("Odd");
+		}
+		
+		//Approach 2
+		
 	}
 }
 
