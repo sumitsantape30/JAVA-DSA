b9f257836de1dfcb88c3f39de5b1fd8f1f848fed
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-08 22:06:29 +0530

    Update A1. EvenOdd.java

--- A3. Conditionals and Loops/A1. Conditionals/A1. EvenOdd.java
+++ A3. Conditionals and Loops/A1. Conditionals/A1. EvenOdd.java
@@ -6,19 +6,51 @@ public class EvenOdd {
 
 	public static void main(String[] args) {
 		
-		// Approach 1
+// Approach 1-------------------------------------------------
 		Scanner s = new Scanner(System.in);
-		int n=s.nextInt();
-		int rem= n%2;
 		
-		if(rem==0){
+		int n=s.nextInt();
+		int rem = n%2 ;
+		
+		if( rem == 0 ){
 			System.out.println("Even");
 		}else{
 			System.out.println("Odd");
 		}
+		//Input: 5
+               // Output: Odd
+			
 		
-		//Approach 2
+//Approach 2--------------------------------------------------
+		Scanner s= new Scanner(System.in);
 		
+		int n=s.nextInt();
+		int rem = n%2;
+		
+		if(rem == 0) {
+			System.out.println( "True" );
+		}
+		System.out.println("Odd");
+		//Input 1    :5
+                // Output 1  : Odd
+		// Input 2   : 8
+                // Output 2  : True
+                //            Odd
+		
+//Approach 3-------------------------------------------------
+		Scanner s= new Scanner(System.in);
+		
+		int n=s.nextInt();
+		int rem= n%2;
+		if(rem == 0) {
+			System.out.println("Even");
+			return;
+		}
+		System.out.println("Odd");
+		//Input 1    : 7
+                // Output 1  : Odd
+		// Input 2   : 8
+                // Output 2  :Even	
 	}
 }
 
