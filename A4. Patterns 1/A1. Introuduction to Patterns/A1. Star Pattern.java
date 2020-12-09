1b92d1d7e9861738496f29206ee337d7147ccef4
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-09 21:20:17 +0530

    Create A1. Star Pattern.java

---
+++ A4. Patterns 1/A1. Introuduction to Patterns/A1. Star Pattern.java
@@ -0,0 +1,22 @@
+package patterns;
+import java.util.Scanner;
+
+public class p1_square {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int i=1;
+		while(i<=n)
+		{
+			int j=1;
+			while(j<=n)
+			{
+				System.out.print("4");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
