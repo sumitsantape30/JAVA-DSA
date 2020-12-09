b63a7c45f63e1c938121b3e78482b7c8762e02a8
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-09 23:36:21 +0530

    Create A1. Triangular Pattern 1.java

---
+++ A4. Patterns 1/A3. Triangular Patterns/A1. Triangular Pattern 1.java
@@ -0,0 +1,29 @@
+package patterns;
+import java.util.Scanner;
+public class p5_triangular {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int i=1;
+		while(i<=n)
+		{
+			int j=1;
+			while(j<=i)
+			{
+				System.out.print(j);
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
+
+Input: 4
+Output: 
+1
+12
+123
+1234
+
