05b8c1b493a9d68cd223aa93ef6c85c70617db79
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-10 19:10:45 +0530

    Create A2. Isosceles Star Pattern.java

---
+++ A5. Patterns 2/A3. Isosceles Triangle/A2. Isosceles Star Pattern.java
@@ -0,0 +1,43 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class IsoscelesStarPattern {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+
+		int i = 1;
+		while (i <= n) {
+			int space = 1;
+			while (space <= n - i) {
+				System.out.print(" ");
+				space++;
+			}
+
+			int j = 1;
+			while (j <= i) {
+				System.out.print("*");
+				j++;
+			}
+
+			j = 1;
+			while (j <= i - 1) {
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+
+}
+
+Input:4
+Output:
+   *
+  ***
+ *****
+*******
