16caf8b03cd3a8eee37925c7eda7d710c5f6461c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-10 19:03:48 +0530

    Update A1. Isosceles Number Pattern.java

--- A5. Patterns 2/A3. Isosceles Triangle/A1. Isosceles Number Pattern.java
+++ A5. Patterns 2/A3. Isosceles Triangle/A1. Isosceles Number Pattern.java
@@ -2,11 +2,13 @@ package patterns;
 
 import java.util.Scanner;
 
-public class IsoscalesTriangle {
+public class IsoscelesTrianglePattern {
 
 	public static void main(String[] args) {
+
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
+
 		int i = 1;
 		while (i <= n) {
 			int space = 1;
@@ -14,24 +16,30 @@ public class IsoscalesTriangle {
 				System.out.print(" ");
 				space++;
 			}
+
 			int j = 1;
-			int p = 1;
 			while (j <= i) {
-				System.out.print(p);
-				p++;
+				System.out.print(j);
 				j++;
 			}
+
 			j = 1;
-			p = i - 1;
+			int p = i - 1;
 			while (j <= i - 1) {
 				System.out.print(p);
-				p++;
-				i++;
+				p--;
+				j++;
 			}
+
 			System.out.println();
 			i++;
 		}
 	}
 }
 
-Input:
+Input: 4
+Output:
+   1
+  121
+ 12321
+1234321
