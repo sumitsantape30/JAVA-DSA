84aed807692e75be9b862c53c45a19760bae70ec
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-10 17:49:53 +0530

    Create A2. Inverted Star Pattern(left).java

---
+++ A5. Patterns 2/A2. Inverted Triangle/A2. Inverted Star Pattern(left).java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class invertedtriangle {
+
+	public static void main(String[] args) {
+  Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+
+		int i = 1;
+		while (i <= n) {
+			int space = 1;
+			while (space <= i) {
+				System.out.print(" ");
+				space++;
+			}
+
+			int j = 1;
+			while (j <= n - i + 1) {
+				System.out.print("*");
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
+
+Input:5
+Output:
+ *****
+  ****
+   ***
+    **
+     *
+
