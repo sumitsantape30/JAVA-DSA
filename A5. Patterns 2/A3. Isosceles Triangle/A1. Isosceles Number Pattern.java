a45db006b51fae6aef788738bfd5313c2f16d76b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-10 17:47:20 +0530

    Create A1. Isosceles Number Pattern.java

---
+++ A5. Patterns 2/A3. Isosceles Triangle/A1. Isosceles Number Pattern.java
@@ -0,0 +1,37 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class IsoscalesTriangle {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1;
+		while (i <= n) {
+			int space = 1;
+			while (space <= n - i) {
+				System.out.print(" ");
+				space++;
+			}
+			int j = 1;
+			int p = 1;
+			while (j <= i) {
+				System.out.print(p);
+				p++;
+				j++;
+			}
+			j = 1;
+			p = i - 1;
+			while (j <= i - 1) {
+				System.out.print(p);
+				p++;
+				i++;
+			}
+			System.out.println();
+			i++;
+		}
+	}
+}
+
+Input:
