9e8ad3942f5d9e54f34394d022fc6c527f91580e
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-10 14:58:02 +0530

    Create A1. Alpha Pattern.java

---
+++ A4. Patterns 1/A4. Character Patterns/A3. Assignments/A1. Alpha Pattern.java
@@ -0,0 +1,30 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class AlphaPattern {
+
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		int i = 1;
+		while (i <= n) {
+			int j = 1;
+			while (j <= i) {
+				System.out.print((char) ('A' + i - 1));
+				j++;
+			}
+			System.out.println();
+			i++;
+		}
+
+	}
+
+}
+
+Input:4
+Output: 
+A
+BB
+CCC
+DDDD
