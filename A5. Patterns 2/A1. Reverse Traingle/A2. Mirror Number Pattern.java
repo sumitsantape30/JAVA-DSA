077839e0667d9aef023d50c6ae83e9c94af95e83
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-10 16:37:56 +0530

    Update A2. Mirror Number Pattern.java

--- A5. Patterns 2/A1. Reverse Traingle/A2. Mirror Number Pattern.java
+++ A5. Patterns 2/A1. Reverse Traingle/A2. Mirror Number Pattern.java
@@ -38,31 +38,27 @@ import java.util.Scanner;
 public class MirrosNumPattern {
 
 	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);
-		int n=s.nextInt();
-		int i=1;
-		int val=1;
-		while(i<=n)
-		{
-			int j=1;
-			while(j<=n-i)
-			{
+
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+
+		int i = 1;
+
+		while (i <= n) {
+			int space = 1;
+			while (space <= n - i) {
 				System.out.print(" ");
-				j++;
+				space++;
 			}
-			
-			j=1;
-			while(j<=i)
-			{
-				System.out.print(val);
-				val++;
+
+			int j = 1;
+			while (j <= i) {
+				System.out.print(j);
 				j++;
 			}
 			System.out.println();
 			i++;
-			
+
 		}
-
 	}
-
 }
