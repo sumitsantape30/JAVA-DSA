1317d34642f9d5d8fa1e7ace52f7e375825be369
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-11 16:17:49 +0530

    Create A3. Guess the output 2.java

---
+++ A6.  Operators and For Loop/A1. Operators (Bitwise, Increment and Decrement)/A3. Assignment/A3. Guess the output 2.java
@@ -0,0 +1,20 @@
+Guess the output
+
+class Solution {
+    public static void main(String args[]) 
+    {   
+    int x = 15;
+    int y = x++;
+    int  z = ++x;
+    System.out.println(y +" " + z);
+    }
+}
+
+Options
+15 16
+16 17
+15 17 
+16 16
+
+Correct Answer
+15 17 
