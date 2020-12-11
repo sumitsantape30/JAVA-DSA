f82e7f492c1fa61937f2b1d80a0797686528cb03
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-11 16:08:45 +0530

    Update A. Bitwise Operators.java

--- A6.  Operators and For Loop/A1. Bitwise Operators/A. Bitwise Operators.java
+++ A6.  Operators and For Loop/A1. Bitwise Operators/A. Bitwise Operators.java
@@ -4,15 +4,18 @@ public class BitwiseOperator {
 
 	public static void main(String[] args) {
 
-		System.out.println(2 & 3); //Bitwiae And
+		System.out.println(2 & 3); //Bitwise and
 		System.out.println(2 | 3); // Bitwise or
 		System.out.println(~2);    // not of 2 (negation)
 		System.out.println(2 ^ 3); // Exclusive XOR
 		
 		System.out.println(34 << 1); //Left Shift Operator (34*2=68) (It multiplies by 2 for each shift)
 		System.out.println(34 >> 1); //Right Shift Operator (34/2=17) (It divides by 2 for each shift)
-
+       
+		System.out.println( -4 << 1);
+		System.out.println( -4 >> 1);
 	}
+
 }
 
 Output:
@@ -22,4 +25,5 @@ Output:
 1
 68
 17
-
+-8
+-2
