17813609746bedb9ec2d896e9be1284cbc6c77cb
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-11 19:32:55 +0530

    Update A2. Increment and Decrement Operators.java

--- A6.  Operators and For Loop/A1. Operators (Bitwise, Increment and Decrement)/A2. Increment and Decrement Operators.java
+++ A6.  Operators and For Loop/A1. Operators (Bitwise, Increment and Decrement)/A2. Increment and Decrement Operators.java
@@ -3,21 +3,31 @@ public class IncrementDecrement {
 
 	public static void main(String[] args) {
 		int a = 10;
-		System.out.println(a++);
+		//System.out.println(++a); //output : 11
+		System.out.println(a++); //Output : 10
 		a++;
 		++a;
-		System.out.println(a);
+		System.out.println(a); //Output: 13
+//========================================================================
+	        int a=10;
+		int b= a++; //Increment honese pehle a ki old value b mai store hogi then increment hoga
+		System.out.println(b); // Output: 10 
+		System.out.println(a); // Output: 11
+//========================================================================
+		int a=10;
+	        int b = ++a;
+		System.out.println(b); //Output: 11
+		System.out.println(a); // Output: 11
 		
-		int b = ++a;
-		System.out.println(b);
-		System.out.println(a);
+// All THE CONCEPTS FOR DRECREMENT OPERATOR ARE SIMILAR TO INCREMENT OPERATOR.
 		
+//======================================================================
 		a = 4;
 		a *= 2;
-		System.out.println(a);
+		System.out.println(a); //Output: 8
 		
 		a ^= 2;
-		System.out.println(a);
+		System.out.println(a); //
 	
 		int ans2 = 2 + 3 * 4;
 		System.out.println(ans2);
