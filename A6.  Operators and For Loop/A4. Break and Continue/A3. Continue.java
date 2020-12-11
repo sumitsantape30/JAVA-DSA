66efd14f68a1a44e90e291afd3af7ca218c47b65
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-12 00:04:10 +0530

    Update A3. Continue.java

--- A6.  Operators and For Loop/A4. Break and Continue/A3. Continue.java
+++ A6.  Operators and For Loop/A4. Break and Continue/A3. Continue.java
@@ -3,15 +3,39 @@ package break_continue;
 public class ContinueDemo {
 
 	public static void main(String[] args) {
-//		int i = 1;
-//		while (i <= 10) {
-//			if (i == 5) {
-//				i++;
-//				continue;
-//			}
-//			System.out.println(i);
-//			i = i + 1;
-//		}
+
+// Continue statement in while loop-----------------------------
+		
+                int n=10;
+		int i=1;
+		while( i<= n) {
+			if( i== 5) {
+				i++; // bina iss i++ ke it will give output as break gives
+				continue;
+			}
+			System.out.println(i);
+			i++;
+		}
+		
+Output with middle i++:
+1
+2
+3
+4
+6
+7
+8
+9
+10
+
+Output without middle i++;
+1
+2
+3
+4
+
+		
+// Continue statement in for loop-------------------------------
 		
 		for (int k = 1; k <= 10; k++) {
 			if (k == 5) {
@@ -19,7 +43,18 @@ public class ContinueDemo {
 			}
 			System.out.println(k);
 		}
-		
+
+Output of for loop:
+1
+2
+3
+4
+6
+7
+8
+9
+10
+
 	}
 
 }
