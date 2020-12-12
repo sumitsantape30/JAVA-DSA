4b13e5c2df1f910763fc266405b943aae2e0e2da
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-12 17:45:29 +0530

    Create A4. Binary to decimal.java

---
+++ A6.  Operators and For Loop/A6. Assignments/A4. Binary to decimal.java
@@ -0,0 +1,50 @@
+Binary to decimal
+
+Given a binary number as an integer N, convert it into decimal and print.
+
+Input format :
+An integer N in the Binary Format
+Output format :
+Corresponding Decimal number (as integer)
+
+Constraints :
+0 <= N <= 10^9
+
+Sample Input 1 :
+1100
+Sample Output 1 :
+12
+
+Sample Input 2 :
+111
+Sample Output 2 :
+7
+
+Code:
+
+package Assignments;
+
+import java.util.Scanner;
+
+public class DecimalToBinary {
+
+	public static void main(String[] args) {
+
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+
+		int ans = 0;
+		int pv = 1;
+		int fv = 0;
+
+		while (n != 0) {
+			fv = n % 10;
+			ans = ans + (fv * pv);
+			n = n / 10;
+			pv = 2 * pv;
+		}
+
+		System.out.println(ans);
+	}
+
+}
