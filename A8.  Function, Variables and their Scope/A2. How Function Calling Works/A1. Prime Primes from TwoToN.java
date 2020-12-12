5ded97f39f5184934f2c0297d884cb3afe822a04
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-12 22:35:17 +0530

    Create A1. Prime Primes from TwoToN.java

---
+++ A8.  Function, Variables and their Scope/A2. How Function Calling Works/A1. Prime Primes from TwoToN.java
@@ -0,0 +1,62 @@
+package Practice;
+
+import java.util.Scanner;
+
+public class primeNumbers {
+
+	public static boolean isPrime(int n) {
+
+		int d = 2;
+		while (d <= n - 1) {
+			if (n % d == 0) {
+				return false;
+			}
+			d++;
+		}
+		return true;
+	}
+
+	public static void printPrime(int n) {
+
+		for (int i = 2; i <= n; i++) {
+			boolean isIPrime = isPrime(i);
+
+			if (isIPrime) {
+				System.out.println(i);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		printPrime(100);
+	}
+
+}
+
+Output:
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
