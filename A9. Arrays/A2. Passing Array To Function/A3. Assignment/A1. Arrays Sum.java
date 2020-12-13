80dcb1b96c09e6d31b2ae3ea4e6247b8e216831f
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-13 18:10:03 +0530

    Update A1. Arrays Sum.java

--- A9. Arrays/A2. Passing Array To Function/A3. Assignment/A1. Arrays Sum.java
+++ A9. Arrays/A2. Passing Array To Function/A3. Assignment/A1. Arrays Sum.java
@@ -35,3 +35,49 @@ Sample Output 2:
 60
 
 Code:
+
+package Restart;
+
+import java.util.Scanner;
+
+public class Practice {
+
+	public static int[] takeInput() {
+
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+
+		int arr[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			arr[i] = s.nextInt();
+		}
+		return arr;
+	}
+
+	public static void print(int arr[]) {
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+
+	public static int arraySum(int arr[]) {
+
+		int sum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+		}
+		return sum;
+	}
+
+	public static void main(String[] args) {
+
+		int arr[] = takeInput();
+		print(arr);
+		int sum = arraySum(arr);
+		System.out.println(sum);
+	}
+}
+
