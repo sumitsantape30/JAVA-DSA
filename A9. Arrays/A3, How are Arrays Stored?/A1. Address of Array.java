5b6c4aaedcb122bfd8ef2fab2fb840bad5b359ca
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-14 01:12:43 +0530

    Create A1. Address of Array.java

---
+++ A9. Arrays/A3, How are Arrays Stored?/A1. Address of Array.java
@@ -0,0 +1,14 @@
+package HowAreArraysStored;
+
+public class HowAreArrayStored {
+
+	public static void main(String[] args) {
+
+		int arr[] = new int[5];
+		System.out.println(arr);// array, memory location mai jis address pe store hai woh address output dega.
+
+	}
+
+}
+
+Output: [I@15db9742
