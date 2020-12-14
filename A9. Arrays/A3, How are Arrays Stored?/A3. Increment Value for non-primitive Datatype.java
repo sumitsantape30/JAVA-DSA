e6a17a0160338f37e2475ee4eaf180cb771c0fa6
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-14 16:57:49 +0530

    Update A3. Increment Value for non-primitive Datatype.java

--- A9. Arrays/A3, How are Arrays Stored?/A3. Increment Value for non-primitive Datatype.java
+++ A9. Arrays/A3, How are Arrays Stored?/A3. Increment Value for non-primitive Datatype.java
@@ -7,7 +7,7 @@ public class HowAreArrayStored {
 	public static void incrementArr(int arr[]) {
 
 		for (int i = 0; i < arr.length; i++) {
-			arr[i]++;
+			arr[i]++; //incrementArr and main wala arr ek hi array ko refer kar rha hai so changes hoga hi array and wahi incremented array print hoga 
 		}
 	}
 
@@ -22,14 +22,70 @@ public class HowAreArrayStored {
 		int arr[] = { 1, 2, 3, 4, 5 };
 		incrementArr(arr);
 		print(arr);
+Output: 2 3 4 5 6 
 
+//=======================================================================================================================
+	
+package HowAreArraysStored;
+
+import java.util.Scanner;
+
+public class HowAreArrayStored {
+
+	public static void incrementArr( int arr[]) {
+		 arr = new int[5];
+		for( int i=0; i< arr.length; i++) {
+			arr[i]++;    			
+		}
+		
+	}
+	
+	public static void print( int arr[]) {
+		for( int i=0; i<  arr.length ; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 2, 3, 4, 5 };
+		incrementArr(arr);
+		print(arr);
+
+	}
+}
+		
+Output: 1 2 3 4 5 
+	
+//=========================================================================================================================================
+		
+		public static int[] incrementArr( int arr[]) {
+		 arr = new int[5];
+		for( int i=0; i< arr.length; i++) {
+			arr[i]++;    			
+		}
+		return arr;
+	}
+	
+	public static void print( int arr[]) {
+		for( int i=0; i<  arr.length ; i++) {
+			System.out.print(arr[i]+" ");
+		}
+	}
+	
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 2, 3, 4, 5 };
+		int incremented[]= 	incrementArr(arr);
+		print(incremented);
+
+Output: 1 1 1 1 1 
+		
+//==============================================================================================================================
+		
+		
 	}
 
 }
 
-Output: 
-2
-3
-4
-5
-6
+
