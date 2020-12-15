790c2187fc3564848d9f08d94c2f042b86a41e12
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-15 18:13:33 +0530

    Update A6. Sort 0 1.java

--- A9. Arrays/A4. Assignment/A6. Sort 0 1.java
+++ A9. Arrays/A4. Assignment/A6. Sort 0 1.java
@@ -39,3 +39,72 @@ Sample Output 2:
 0 0 0 1 1 
 
 Code:
+
+import java.util.*;
+public class Solution 
+{  
+
+    public static void sortZeroesAndOne(int[] arr) 
+    {
+       
+//         int j = -1; 
+//         for (int i = 0; i < arr.length; i++) 
+//         { 
+  
+//             if (arr[i] < 1) 
+//             { 
+                
+//                 j++; 
+//                 swap(arr, j, i);
+//         	}
+//         }
+       int j = arr.length-1;
+       for(int i = 0; i < j;  )
+       {
+       	   if(arr[i] == 0)
+       	   {
+       	      i++;
+       	   } 
+                   else if(arr[i] == 1)
+       	           {
+       		                  if(arr[j] == 1 )
+       		                  {
+       			                    j--;
+       		                  } 
+                                     else if(arr[j] == 0)
+       		                         {
+       			                          swap(arr,i, j);
+       		                          }
+       	            }
+        }
+        
+    }
+    public static void swap(int arr[], int i, int j)
+    {
+    	int temp = arr[i];
+    	arr[i] = arr[j];
+    	arr[j] = temp;
+    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t != 0)
+        {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for(int i = 0; i < n; i++)
+            {
+            	a[i] = sc.nextInt();
+            }
+           
+        sortZeroesAndOne(a);
+        for(int i = 0; i < a.length; i++)
+        {
+        	System.out.print(a[i]+" ");
+        }
+        t--;
+        
+        }        sc.close();
+
+    }
+}
