b1cff2328c6e77fe406a33e7fb5a23547842ed56
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-18 20:38:10 +0530

    Create A3. StudentUse.java

---
+++ B13. OOPS 1/A2. Access Modifiers/A2. Constructors/A4. Important Keywords/A2. This/A3. StudentUse.java
@@ -0,0 +1,22 @@
+package Restart;
+
+import java.util.Scanner;
+
+public class StudentUse {
+
+	public static void main(String[] args) {
+
+		
+		Student s1= new Student("Sumit",65); 
+		s1.print(); 
+	
+	    Student s2= new Student("Santape",65); 
+	    s2.print();  
+	    
+	}
+
+}
+
+Output:
+Sumit 65
+Santape 65
