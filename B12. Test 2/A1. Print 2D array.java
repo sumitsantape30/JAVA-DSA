a5458d735d9c5fec10fa2f9491b24227a27eb17a
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-18 09:02:38 +0530

    Create A1. Print 2D array.java

---
+++ B12. Test 2/A1. Print 2D array.java
@@ -0,0 +1,43 @@
+Print 2D array
+
+Given a 2D integer array with n rows and m columns. Print the 0th row from input n times, 1st row n-1 times…..(n-1)th row will be printed 1 time.
+
+Input format :
+Line 1 : No of rows(n) & No of columns(m) (separated by space)
+
+Line 2 : Row 1 elements (separated by space)
+
+Line 3 : Row 2 elements (separated by space)
+
+Line 4 : and so on
+
+Sample Input :
+3 3
+1    2    3
+4    5    6
+7    8    9
+Sample Output :
+1    2    3
+1    2    3
+1    2    3
+4    5    6
+4    5    6
+7    8    9
+
+Code:
+
+public class solution {
+	public static void print2DArray(int input[][]) {
+		int k = input.length;
+		for( int i = 0; i < input.length ; i++ ) {
+			for( int l = k ; l > 0; l-- ) {
+				for( int j = 0; j < input[i].length; j++ ) {
+					System.out.print( input[i][j] + " ");
+				}
+				System.out.println();
+			}
+			k--;
+		}
+
+	}
+}
