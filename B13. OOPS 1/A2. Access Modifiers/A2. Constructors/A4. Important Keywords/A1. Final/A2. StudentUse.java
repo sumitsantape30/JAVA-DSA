8dc8d83d54a1f47b9f21644bdadc1a900b56d749
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-18 19:07:03 +0530

    Create A2. StudentUse.java

---
+++ B13. OOPS 1/A2. Access Modifiers/A2. Constructors/A4. Important Keywords/A1. Final/A2. StudentUse.java
@@ -0,0 +1,23 @@
+package Restart;
+
+import java.util.Scanner;
+
+public class StudentUse {
+
+	public static void main(String[] args) {
+
+		
+		Student s1= new Student("Sumit",65);
+		s1.print();
+		System.out.println(s1.getRollNumber());
+		
+	}
+
+}
+
+Output:
+
+Sumit 65
+65
+Sumit 65
+Santape 65
