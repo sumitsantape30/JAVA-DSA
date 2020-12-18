d9dced6bda59cd3a1d35da587323580a400ea261
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-18 11:41:47 +0530

    Create A1. Student (class).java

---
+++ B13. OOPS 1/A2. Access Modifiers/A1. Access Modifiers/A1. Student (class).java
@@ -0,0 +1,24 @@
+package Restart;
+
+public class Student {
+
+	//yeh student class/template ki properties hai
+	String name;
+	 public int rollNo;
+	 
+	 
+	 // humara roll no private hai so usko class ke bahar koi access nhi kr skta neigther change kr skta to private krke fayda kya,
+	 // hum isme kuch functionalities add karenge
+	 
+	 public void setRollNumber( int rn) { //this is the way through which we can change the roll no, yeh function public hai so isko class ke bahar access krke isme input deke roll number change kr skte hai
+		 
+		 if( rn <= 0) { // rollNo private rakhne ka fayda is, mai yeh control rakh skta hu ki user negtive value na dede, mai yaha aise condition laga skta
+			 return;
+		 }
+		 rollNo= rn;
+	 }
+	 
+	 public int getRollNumber( ) { //yeh function public hai so class ke bahar se accessible hai and yahi function within the class bhi hai so woh(getRollNumber function) rollNo ko access kr skta hai.
+		 return rollNo;
+	 }
+}
