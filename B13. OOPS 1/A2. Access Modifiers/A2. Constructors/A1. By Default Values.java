2ec96c8a63c91d9f9ff176b038f657bf1cfa1f96
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-18 14:31:49 +0530

    Create A1. By Default Values.java

---
+++ B13. OOPS 1/A2. Access Modifiers/A2. Constructors/A1. By Default Values.java
@@ -0,0 +1,22 @@
+public class Student {
+
+	//by default when you create a student all the properties that you have here gets initialized by zero, string ke case mai null.
+	       String name;
+	 public int rollNo;
+   
+   }
+   
+   public class StudentUse {
+
+	public static void main(String[] args) {
+
+		
+		Student s1= new Student(); 
+		
+		//by default when you create a object all the properties that you have here gets initialized by zero, string ke case mai null.
+		System.out.println(s1.name); //Output: null
+		System.out.println(s1.getRollNumber()); //Output: 0
+		
+	}
+
+}
