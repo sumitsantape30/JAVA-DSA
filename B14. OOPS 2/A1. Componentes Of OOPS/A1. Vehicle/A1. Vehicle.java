92f0a86b4fca0925def677694728be9de5b8ef0b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-19 14:49:46 +0530

    Update and rename B14. OOPS 2/A1. Componentes Of OOPS/A1. to B14. OOPS 2/A1. Componentes Of OOPS/A1. Vehicle/A1. Vehicle.java

---
+++ B14. OOPS 2/A1. Componentes Of OOPS/A1. Vehicle/A1. Vehicle.java
@@ -0,0 +1,14 @@
+package oops2;
+
+public class Vehicle {
+
+	//yaha humne vehicle specific chize dal di
+	String color;
+	int maxSpeed;
+	
+	public void print() {
+		System.out.println("Vehicle Color :"+color); //Output: null
+		System.out.println("Vehicle Color :"+maxSpeed); // Color: 0
+	}
+}
+
