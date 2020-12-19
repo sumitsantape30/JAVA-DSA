c9834e7bf908222e498f11e1e47f66cdfb27b912
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-19 14:50:35 +0530

    Create A2. Car.java

---
+++ B14. OOPS 2/A1. Componentes Of OOPS/A1. Vehicle/A2. Car.java
@@ -0,0 +1,9 @@
+package oops2;
+
+public class Car extends Vehicle { // inherit ke bas Vehicle ke saare functions,properties ab Car mai agye
+	//agar muje inheritance nhi ata hota to mai yaha bhi color, maxSpeed banata kyuki car keliye bhi chahiye yeh chize
+	//yaha mai car specific chize dal dunga
+	int numGears;
+	int isConvertible;
+	
+}
