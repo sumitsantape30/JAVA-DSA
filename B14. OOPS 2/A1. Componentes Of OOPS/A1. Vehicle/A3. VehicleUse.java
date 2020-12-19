668f8a4e1b35264991e477741a48a14e8a9abe8b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-19 14:51:27 +0530

    Create A3. VehicleUse.java

---
+++ B14. OOPS 2/A1. Componentes Of OOPS/A1. Vehicle/A3. VehicleUse.java
@@ -0,0 +1,24 @@
+package Maps;
+
+public class VehicleUse {
+
+	public static void main(String[] args) {
+
+		Vehicle v= new Vehicle();
+		v.print();
+		
+		Car c= new Car();
+		c.numGears= 10; //yaha numGears access kr pa rhe coz woh default hai so same package mai access set kr pare
+        c.color= "Blue"; //hum vehicle ki property Car keliye use karre coz car extends Vehicle
+        c.print(); //hum vehicle ke functions bhi apne liye use kr skte hai
+        
+	}
+
+}
+
+Output:
+
+Vehicle Color :null
+Vehicle Color :0
+Vehicle Color :Blue
+Vehicle Color :0
