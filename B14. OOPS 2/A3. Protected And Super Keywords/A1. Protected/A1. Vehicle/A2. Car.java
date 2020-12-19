42a1a41355f89bf7f023f362bea24ebfdf34aa39
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-19 17:27:33 +0530

    Create A2. Car.java

---
+++ B14. OOPS 2/A3. Protected And Super Keywords/A1. Protected/A1. Vehicle/A2. Car.java
@@ -0,0 +1,17 @@
+package Maps;
+
+public class Car extends Vehicle { 
+	// car ke andar vehicle ke private data memebr bhi inherit honge but directly access nhi kr skti
+	//yaha mai car specific chize dal dunga
+	int numGears;
+	int isConvertible;
+	
+	public void print() { //printCar() se print() kiya hua h
+		System.out.println("Car Color : "+color); //hum color and maxSpeed ko access kr pa rhe h
+		System.out.println("Car maxSpeed : "+getmaxSpeed()); //car bhi getmaxSpeed access kr skti hai coz woh function public h and then getmaxSpeed function maxspeed ko access kr skta h
+		System.out.println("numGears : "+numGears);
+		System.out.println("isConvertible : "+isConvertible);
+
+	}
+
+}
