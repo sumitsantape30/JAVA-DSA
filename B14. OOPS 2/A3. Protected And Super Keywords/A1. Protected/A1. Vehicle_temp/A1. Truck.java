c8ec98543360c81234aaf2a0891be7f00e38b7d6
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-19 17:30:18 +0530

    Create A1. Truck.java

---
+++ B14. OOPS 2/A3. Protected And Super Keywords/A1. Protected/A1. Vehicle_temp/A1. Truck.java
@@ -0,0 +1,15 @@
+package Vehicle_temp;
+
+import vehicle.Vehicle;
+
+public class Truck extends Vehicle{
+
+	int maxLoadingCapacity;
+	
+	public void print() {
+		System.out.println("maxLoadingCapacity : "+maxLoadingCapacity);
+		System.out.println("color : "+color); //jo data memeber Vehicle mai hai jo dusre package mai hai usko access kr pa rhe coz woh ab protected hai
+	    System.out.println("Speed : "+getmaxSpeed()); //yeh function mera public hai so access kr skte hai
+	   //getmaxSpeed through ke jake maine maxSpeed access kiya.
+	}
+}
