4a80c23e7f2806a9fe2c90e0eab7c88d7d723c0b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-19 21:42:54 +0530

    Create A3, VehicleUse.java

---
+++ B14. OOPS 2/A4. Inheritance And Constructors/A2. How constructor runs?/A3, VehicleUse.java
@@ -0,0 +1,21 @@
+package Maps;
+
+public class VehicleUse {
+
+	public static void main(String[] args) {
+
+		//Vehicle v= new Vehicle(10); //jab mai yeh object create kar rha hu to vehicle ka constructor call hoga.
+		// Output: Vehicle Constructor
+		
+      //when you created car object this vehicle object was also getting created, mere isme automatic vehicle crete ho rha hai to woh default constructor( new vehicle()) call karneki koshish kr rha hai
+		// since vehicle object is getting created but humne wahaka default constructor gayab kardiya
+		Car c= new Car(100); //jab mai yeh object create kar rha hu to car ka constructor call hoga.
+	   //Output:Vehicle Constructor
+	   //Car Constructor
+		
+		//Point to remember:
+		// 1. derived class ka constructor call krte hai to pehle derived class ka then super() class ka bhi constructor call hota hai.
+		// 2. default constructor hai to thik hai, default constructor nhi hai to tumhe specifically call karna padega super() keyword use krke. 
+	}
+
+}
