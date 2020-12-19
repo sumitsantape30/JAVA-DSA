50e3368c4dca0d3c47c1b8021a2771ea347fe031
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-19 17:45:40 +0530

    Create A1. Vehicle.java

---
+++ B14. OOPS 2/A3. Protected And Super Keywords/A2. Super/A1. Vehicle/A1. Vehicle.java
@@ -0,0 +1,20 @@
+package Maps;
+
+public class Vehicle {
+  
+	protected String color; //protected karne se I can access color in other package also
+	private int maxSpeed; 
+	
+	public int getmaxSpeed() {
+		return maxSpeed;
+	}
+	
+	public void setmaxSpeed( int maxSpeed) {
+		this.maxSpeed= maxSpeed;
+	}
+	
+	public void print() {
+		System.out.println("Vehicle Color :"+color); //Output: null
+		System.out.println("Vehicle maxSpeed :"+maxSpeed); // Color: 0
+	}
+}
