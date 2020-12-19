ade3dc0f673cdfcadd1e27197e4b1293e6775686
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-19 16:10:17 +0530

    Create A1. Vehicle.java

---
+++ B14. OOPS 2/A2. Inheritance and Private Members/A1. Vehicle.java
@@ -0,0 +1,23 @@
+package Maps;
+
+public class Vehicle {
+
+	String color;
+	private int maxSpeed; //maxSpeed ko private kiya to maxSpeed ko iss class ke bahar access nhi krskte so iss class ke bahar bhi access karwani ke indirect way is create getters and setters
+	
+	//Car directly maxSpeed ko access nhi kr skti coz private hai, but car getmaxSpeed ko call kr skti hai aur fir wahase getmaxSpeed maxSpeed ko access kr skta hai. 
+	
+	
+	public int getmaxSpeed() {
+		return maxSpeed;
+	}
+	
+	public void setmaxSpeed( int maxSpeed) {
+		this.maxSpeed= maxSpeed;
+	}
+	
+	public void print() {
+		System.out.println("Vehicle Color :"+color); //Output: null
+		System.out.println("Vehicle maxSpeed :"+maxSpeed); // Color: 0
+	}
+}
