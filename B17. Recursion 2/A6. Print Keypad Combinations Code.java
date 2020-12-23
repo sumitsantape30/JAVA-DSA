c099b5fc98d7058675000274a14a7502b0bd9a14
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-23 11:24:31 +0530

    Update A6. Print Keypad Combinations Code.java

--- B17. Recursion 2/A6. Print Keypad Combinations Code.java
+++ B17. Recursion 2/A6. Print Keypad Combinations Code.java
@@ -25,3 +25,50 @@ ce
 cf
 
 Code:
+
+public class solution {
+
+	public static void printKeypad(int input){
+		printKeypad(input, "");
+	}
+    private static void printKeypad(int input, String output) {
+		
+		if(input == 0) {
+				System.out.println(output);
+			     return;
+		}
+		
+		String str = StringArray(input % 10) ; //234 % 10 = 4 ---> ghi
+		for(int i = 0; i < str.length(); i++) {
+		  
+			 printKeypad(input / 10,  str.charAt(i) + output);
+			
+		}
+	       
+	}
+	 private static String StringArray(int n ) {
+	    	
+	   switch(n) {
+	    	case 2:  return  "abc"; 
+	    	
+	    	case 3:  return "def";
+	    		
+	    	case 4:  return "ghi";
+	    		
+	    	case 5:  return "jkl";
+	    		
+	    	case 6:  return  "mno";
+	    		
+	    	case 7:  return "pqrs";
+	    		
+	    	case 8:  return  "tuv";
+	    		
+	    	case 9:  return "wxyz";
+	    	 
+	    	default: break;
+	    	}
+	    	
+	    	return "";
+	    }
+}
+
