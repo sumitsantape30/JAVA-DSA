59df12b86aee03d7ab7c1b2c63423eb11f6589db
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-25 01:48:14 +0530

    Create A2. LinkedListUse.java

---
+++ C20. Linked List 1/A1. Intro to Linked List/A1. Node (Creating Single Node)/A2. LinkedListUse.java
@@ -0,0 +1,33 @@
+package LinkedList;
+
+public class LinkedListUse {
+
+	public static void print(Node<Integer> head) {
+		
+		while(head != null) {
+			System.out.print(head.data+" ");
+			head= head.next;
+		}
+		
+		System.out.println();
+		
+		
+	}
+	
+	public static void main(String[] args) {
+
+		//linkedlist ka ek node create karna matlab uss Node class ka ek object create karna, Node class is template ki LinkedList ke nodes kaise dikhne chahiye 
+		Node<Integer> node1= new Node<Integer>(10); //mera ek node ban gya jiska data 10 hai
+		
+		//node 1 ka data access karne keliye
+		System.out.println(node1.data);
+	    
+		//node 1 ka next access karne keliye
+		System.out.println(node1.next);
+   
+   }
+}
+
+Output:
+10
+null
