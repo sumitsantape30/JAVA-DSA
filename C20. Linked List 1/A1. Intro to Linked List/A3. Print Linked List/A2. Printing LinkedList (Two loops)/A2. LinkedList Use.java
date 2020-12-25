789d7bd1d511c85927b2a56ce5613874c3f34ce1
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-25 12:58:25 +0530

    Update A2. LinkedList Use.java

--- C20. Linked List 1/A1. Intro to Linked List/A3. Print Linked List/A2. Printing LinkedList (Two loops)/A2. LinkedList Use.java
+++ C20. Linked List 1/A1. Intro to Linked List/A3. Print Linked List/A2. Printing LinkedList (Two loops)/A2. LinkedList Use.java
@@ -29,6 +29,7 @@ public class LinkedListUse {
 		// jab head null hoga tab hum first loop ke bahar ayenge, then second loop tak ayenge tab head already null ho chuka hoga so second loop enter hi nhi karenge coz loop condition false hogi
 
 //=========================================================================================================================================================================================================		
+		//if you want ki kuch kam karne ke bad list ko aage bhi use karna hai tumhe, we'll keep a reference of the first node with us in temp.
 		//agar woh head/list hume first loop ke bad bhi use karna hai to first loop ke pehlehi uss head ko temp mai store karke rakh lo
 		
 		Node<Integer> node1 = new Node<>(10);
@@ -47,9 +48,10 @@ public class LinkedListUse {
 
 		System.out.println();
 		
-		while (temp != null) {
-			System.out.print(temp.data + " ");
-			temp = temp.next;
+		head= temp; //yeh likhne ke bad mai use wapas first first pe leke aya
+		while (head != null) {
+			System.out.print(head.data + " ");
+			head = head.next;
 		}
 		
 	}
