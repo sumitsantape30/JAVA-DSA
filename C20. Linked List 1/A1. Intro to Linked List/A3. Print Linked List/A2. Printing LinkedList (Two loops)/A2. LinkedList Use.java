df9bb9927b57fa28152c241d7e48474846911bd3
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-25 12:30:40 +0530

    Create  A2. LinkedList Use.java

---
+++ C20. Linked List 1/A1. Intro to Linked List/A3. Print Linked List/A2. Printing LinkedList (Two loops)/A2. LinkedList Use.java
@@ -0,0 +1,57 @@
+package Restart;
+
+public class LinkedListUse {
+
+	public static void main(String[] args) {
+
+		Node<Integer> node1 = new Node<>(10);
+		Node<Integer> node2 = new Node<>(20);
+		Node<Integer> node3 = new Node<>(30);
+		node1.next = node2;
+		node2.next = node3;
+
+		Node<Integer> head = node1;
+
+		while (head != null) {
+			System.out.print(head.data + " ");
+			head = head.next;
+		}
+
+		System.out.println();
+		
+		while (head != null) {
+			System.out.print(head.data + " ");
+			head = head.next;
+		}
+		
+		//Output: 10 20 30 (sirf first loop ka result ayega second loop ka result nhi ayega)
+		// first wala loop khatam hojayega jab head null hojayega.
+		// jab head null hoga tab hum first loop ke bahar ayenge, then second loop tak ayenge tab head already null ho chuka hoga so second loop enter hi nhi karenge coz loop condition false hogi
+
+//=========================================================================================================================================================================================================		
+		//agar woh head/list hume first loop ke bad bhi use karna hai to first loop ke pehlehi uss head ko temp mai store karke rakh lo
+		
+		Node<Integer> node1 = new Node<>(10);
+		Node<Integer> node2 = new Node<>(20);
+		Node<Integer> node3 = new Node<>(30);
+		node1.next = node2;
+		node2.next = node3;
+
+		Node<Integer> head = node1;
+		Node<Integer> temp= head;
+		
+		while (head != null) {
+			System.out.print(head.data + " ");
+			head = head.next;
+		}
+
+		System.out.println();
+		
+		while (temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.next;
+		}
+		
+	}
+
+}
