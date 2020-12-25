15ee400eed6cd864c46aac999dce42719e2c8ee5
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-25 23:07:14 +0530

    Create A2. MidePoint.java

---
+++ C21. Linked List 2/A1. Midpoint of Linked list/A2. MidePoint.java
@@ -0,0 +1,72 @@
+package Restart;
+
+import java.util.Scanner;
+
+public class Practices {
+
+	public static Node<Integer> input() {
+		Node<Integer> head = null, tail = null;
+
+		Scanner s = new Scanner(System.in);
+		int data = s.nextInt();
+
+		while (data != -1) {
+			Node<Integer> newNode = new Node<Integer>(data);
+
+			if (head == null) {
+				head = newNode;
+				tail = newNode;
+			} else {
+				tail.next = newNode;
+				tail = tail.next;
+			}
+			data = s.nextInt();
+		}
+		return head;
+	}
+
+	public static int length(Node<Integer> head) {
+		int length = 0;
+
+		while (head != null) {
+			length++;
+			head = head.next;
+		}
+		return length;
+	}
+
+	public static Node<Integer> midPoint(Node<Integer> head, int length) {
+
+		if (head == null || head.next == null) {
+			return head;
+		}
+
+		// do pointers lenge slow and fast, dono starting mai first node pe honge, slow ek step aage jayega and fast do step aage jayega, jab fast end pe pahuch chuka hoga to slow humara mipoint pe hoga, slow jispe hoga woh answer hoga.
+		Node<Integer> slow = head;
+		Node<Integer> fast = head;
+
+		if (length % 2 == 0) {
+
+			while (fast != null && fast.next.next != null) { // even ki case mai tabtak aage badhate rahenge jabtak fast ke next ka next null nhi ho jata
+				slow = slow.next;
+				fast = fast.next.next;
+			}
+		} else {
+
+			while (fast != null && fast.next != null) { // odd ki case mai tabtak aage badhte rahenge jabtak fast ke next wala null nhi hojata.
+				slow = slow.next;
+				fast = fast.next.next;
+			}
+		}
+		return slow;
+	}
+
+	public static void main(String[] args) {
+
+		Node<Integer> head = input();
+		int length = length(head);
+		Node<Integer> midpoint = midPoint(head, length);
+		System.out.println(midpoint.data);
+	}
+
+}
