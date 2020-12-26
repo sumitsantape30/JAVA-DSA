586c5b7af14ad87f0487d04b2d8b48cfd484b3c5
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-26 14:26:14 +0530

    Create A2. DoubleNode.java

---
+++ C21. Linked List 2/A4. Reverse LL/A3. Reverse Recursively Double  Node/A2. DoubleNode.java
@@ -0,0 +1,12 @@
+package ReverseLL_Recursive;
+
+//humara reversed list ke sirf head se kam nhi ban rha(sirf head rha to list ke end tak pahuchne keliye travel karna pad rha hai), isliye reversed list ka head and tail dono return karayenge
+// function bas ek hi chiz return kar skta hai so I'm warpping it into a class, then iss class ka object return karwayenge taki un dono properties ka access mil paye
+public class DoubleNode { // ek node ke do different nodes store kr rhe hai so DoubleNode nam diya hai 
+
+	Node<Integer> head;
+	Node<Integer> tail;
+
+	//so agar kisi function se multiple chize return karwani hai function se to wrapped those things inside a class
+	// mai DoubleNode ka object banau to uske pas do properties hongi, head and tail.
+}
