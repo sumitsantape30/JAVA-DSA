15daf5fc812c1ceccf769363fa7653e5985f4569
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-29 15:47:40 +0530

    Create A1. BinaryTreeNode.java

---
+++ C26. Binary Trees/A1. BinaryTreeNode/A1. BinaryTreeNode.java
@@ -0,0 +1,13 @@
+
+//yeh ek generic class hogi, mai nhi chahta ki har type ka data store karne keliye bar bar uss datatype keliye separate class banau isliye generic
+public class BinaryTreeNode<T> { //humare ek node mai data rahega and do children ke addresses rahenge, ek left wale child ka aur right wale child ka
+
+	//mai inki visibility restrict nhi karna chahta so public rakhgenge taki koi bhi change kar paye
+	public T data;
+	public BinaryTreeNode<T> left;
+	public BinaryTreeNode<T> right;
+	
+	public BinaryTreeNode(T data) {
+		this.data= data;
+	}
+}
