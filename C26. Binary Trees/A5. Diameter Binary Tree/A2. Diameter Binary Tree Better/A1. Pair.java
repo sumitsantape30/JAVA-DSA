372aee3ba34b8c89bd62bb14071a990b53677489
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2020-12-30 01:01:34 +0530

    Create A1. Pair.java

---
+++ C26. Binary Trees/A5. Diameter Binary Tree/A2. Diameter Binary Tree Better/A1. Pair.java
@@ -0,0 +1,9 @@
+package Questions;
+
+public class Pair<T,V> {
+
+	public T first; //pair ka first T type ka lenge
+	public V second; //and pair ka second V type ka le lunga
+	//public banaya taki access kr skte
+	
+}
