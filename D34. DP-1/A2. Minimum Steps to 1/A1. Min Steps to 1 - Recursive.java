7253c4d5fcb9fdfde70520c39df4095c4caad6cc
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-01-06 15:19:35 +0530

    Update A1. Min Steps to 1 - Recursive.java

--- D34. DP-1/A2. Minimum Steps to 1/A1. Min Steps to 1 - Recursive.java
+++ D34. DP-1/A2. Minimum Steps to 1/A1. Min Steps to 1 - Recursive.java
@@ -35,3 +35,46 @@ Step 2 : n = 6  / 3 = 2
 Step 3 : n = 2 / 2 = 1  
 
 Code:
+
+
+public class TakeNumberToOne {
+	
+	//hum teeno options ko bolenge ki apne apne number of steps leke aao jiske steps sabse minimum honge wahi answer
+	public static int countSteps(int n) { //To count the number of steps 
+		//we are assuming given value will 1 or greater than 1
+		
+		if( n == 1) { //This is basecase : you can say smallest problem we can solve
+			return 0; //agar n=1 hai to 1 ko 1 tak le jane mai hume koi steps perform nhi karne so return number of steps= 0
+		}
+		
+		int op1= countSteps(n-1); //(substract karne ka option humesha rahega) n-1 ko 1 tak lane mai jitne bhi steps lagenge woh 
+		int minSteps= op1; //initially op1 mai jitne steps aaye wahi answer hai humara
+		
+		//explore Option2 : agar 3 se divide hota hai to options 2 bhi nikal lenge
+		if( n % 3 == 0) {
+			int op2= countSteps( n/3 ); //Number of steps to make n/3 to 1
+			if( op2 < minSteps) {
+				minSteps= op2; // agar op2 mai kam steps lage hai than minSteps to minSteps ko update kr dete h	
+			}
+		}
+		
+		//option3
+		if( n % 2 == 0) {
+			int op3= countSteps( n/2 ); // number of steps to take n/2 to one
+			if ( op3 < minSteps ) {
+				minSteps= op3; // agar op3 better hai minSteps se to minSteps ko update krdo	
+			}
+		}
+		
+		//finally mere pas minSteps mai sahi answer hoga
+		return 1+ minSteps; // 1+ isliye kiya coz, jo step humne op1 mai peroform kiya usko bhi total number of steps mai consider karna padega.
+	}
+	
+  public static void main(String[] args) {
+ 
+		int n=10;
+		System.out.println(countSteps(n));
+		
+	}
+
+}
