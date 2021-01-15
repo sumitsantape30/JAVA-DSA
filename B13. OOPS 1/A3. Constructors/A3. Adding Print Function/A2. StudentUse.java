c16d767115489a777efd0021ef96297baa77ab6c
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-01-15 18:46:23 +0530

    Update A2. StudentUse.java

--- B13. OOPS 1/A3. Constructors/A3. Adding Print Function/A2. StudentUse.java
+++ B13. OOPS 1/A3. Constructors/A3. Adding Print Function/A2. StudentUse.java
@@ -11,17 +11,17 @@ public class StudentUse {
 		
 //		System.out.println(s1.name);
 //		System.out.println(s1.getRollNumber());
-	    s1.print(); //Output: Sumit 65
+	        s1.print(); //Output: Sumit 65
 
 	    
-	    Student s2= new Student("Santape",65);
-	    s2.print(); //Output: Santape 65
+	       Student s2= new Student("Santape",65);
+	       s2.print(); //Output: Santape 65
 	    
 	    //multiple constructor
 	 // jab maine multiple constructor banaye means muje student(s1) banane keliye ya to name and roll No dedo (pointing constructur with name and rollNo argument)	ya fir sirf nam dedo, nam to dena hi padega
 
-	    Student s3= new Student("Amit");
-		s3.print(); //Output: Amit 0
+	       Student s3= new Student("Amit");
+	       s3.print(); //Output: Amit 0
 
 	}
 
