db217b88c052d49d4c983cc15e0fb294b662c7f3
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-01-15 14:47:20 +0530

    Update A1. Student (class).java

--- B13. OOPS 1/A1. Creating Class And Object/A1. Student (class).java
+++ B13. OOPS 1/A1. Creating Class And Object/A1. Student (class).java
@@ -2,7 +2,7 @@ package Restart;
 
 public class Student {
 
-	//yeh student class/template ki properties hai
+	//yeh student class/template ki properties hai, so every student object will be having these properties
 	String name;
 	int rollNo;
 }
