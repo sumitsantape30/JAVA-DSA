be34f78a12fdf7a242c1f116e335913c043b033e
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-01-16 14:53:50 +0530

    Update A1. Student (class).java

--- B13. OOPS 1/A4. Important Keywords/A3. Static/A1. StaticUse (Not good way)/A1. Student (class).java
+++ B13. OOPS 1/A4. Important Keywords/A3. Static/A1. StaticUse (Not good way)/A1. Student (class).java
@@ -5,7 +5,7 @@ public class Student {
 	
 	//I want to have another data member called numStudents jo maintain karega ki mere student class ke kitne objects ban chuke hai,
 	// pehle object create hoga to numStudents 1 hoga dusra object create hua to numStudents 2 hoga, aur agar 10 objects create hue to numStudents 10 chahiye.
-	// so, I should have only one numStudents integer instead of creating one copy of each object, so we need one numStudent which is shared by everyon.
+	// so, I should have only one numStudents integer instead of creating one copy of each object, so we need one numStudent which is shared by everyone.
 	// When you want only one data that should be shared by everybody we call it static data.
 	
 	String name;
@@ -13,7 +13,7 @@ public class Student {
 	
 	static int numStudents; //static hai so sab share karenge, and numStudents keeps track ki abhitak kitne objects create hue hai
 
-	public Student(String name, int rollNo) {
+	public Student(String name, int rollNo) {// jab bhi ek object create hoga constructor ek bar call hoga so numStudents should be inside the constructor taki jabhi object banaye to increse ho.
 		this.name = name;
 		this.rollNo = rollNo;
 		numStudents++; // everytime a object is created we want to increase our numStudents, so jab bhi object create hoga ekbar constructor call hoga
