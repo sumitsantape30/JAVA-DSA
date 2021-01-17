29d27ad72759048c27ed943c127cee7788b7249b
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-01-17 18:43:33 +0530

    Update A3. VehicleUse.java

--- B14. OOPS 2/A4. Inheritance And Constructors/A1. Object-Constructor Calling/A3. VehicleUse.java
+++ B14. OOPS 2/A4. Inheritance And Constructors/A1. Object-Constructor Calling/A3. VehicleUse.java
@@ -8,10 +8,10 @@ public class VehicleUse {
 		// Output: Vehicle Constructor
 		
 		//hum jab car banayenge to car ke andar vehicle bhi banhega hi.
-		//jab hum derived class ka constructor call krte hai (means jab hum derived class ka object bana rhe) to yeh parent class object bhi banata hai so parent kabhi constructor call hota hai.
+		//jab hum derived class ka constructor call krte hai (means jab hum derived class ka object bana rhe) to yeh parent class ka object bhi banata hai so parent kabhi constructor call hota hai.
 		Car c= new Car(); //jab mai yeh object create kar rha hu to car ka constructor call hoga.
 		//Output:  Vehicle Constructor
-    	//         Car Constructor
+    	//                 Car Constructor
         
        
 	}
