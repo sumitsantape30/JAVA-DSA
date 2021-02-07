9e417bf00a9242e82261b98d51c9a6e4e392b6e6
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-02-07 17:01:30 +0530

    Update A1. Kruskals Algorithm.java

--- D37. Graphs 2/A1. Kruskals Algorithm.java
+++ D37. Graphs 2/A1. Kruskals Algorithm.java
@@ -1,6 +1,6 @@
 Code : Kruskal's Algorithm
 
-	Given an undirected, connected and weighted graph G(V, E) with V number of vertices (which are numbered from 0 to V-1) and E number of edges.
+Given an undirected, connected and weighted graph G(V, E) with V number of vertices (which are numbered from 0 to V-1) and E number of edges.
 Find and print the Minimum Spanning Tree (MST) using Kruskal's algorithm.
 For printing MST follow the steps -
 1. In one line, print an edge which is part of MST in the format - 
@@ -9,7 +9,7 @@ where, v1 and v2 are the vertices of the edge which is included in MST and whose
 2. Print V-1 edges in above format in different lines.
 Note : Order of different edges doesn't matter.
 
-	Input Format :
+Input Format :
 Line 1: Two Integers V and E (separated by space)
 Next E lines : Three integers ei, ej and wi, denoting that there exists an edge between vertex ei and vertex ej with weight wi (separated by space)
 Output Format :
