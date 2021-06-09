578895146cce163c4f0a6b25d8fdcb3759e10a83
Sumit Santape <64180276+sumitsantape30@users.noreply.github.com>
2021-06-09 21:17:01 +0530

    Update A1. Vehicle.java

--- B14. OOPS 2/A4. Inheritance And Constructors/A1. Object-Constructor Calling/A1. Vehicle.java
+++ B14. OOPS 2/A4. Inheritance And Constructors/A1. Object-Constructor Calling/A1. Vehicle.java
@@ -1,19 +1,24 @@
-package Maps;
+package vehicle;
 
-public class Car extends Vehicle { 
+public class Vehicle {
+	protected String color;
+	private int maxSpeed;
 
-	int numGears;
-	int isConvertible;
-	
-	public Car() {
-		System.out.println("Car Constructor");
-	}
-	
-	public void print() { 
-		super.print(); 
-		System.out.println("numGears : "+numGears);
-		System.out.println("isConvertible : "+isConvertible);
-
+	public Vehicle(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+		System.out.println("Vehicle Constructor");
 	}
 
+	public int getmaxSpeed() {
+		return maxSpeed;
+	}
+
+	public void setmaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+
+	public void print() {
+		System.out.println("Color : " + color);
+		System.out.println("maxSpeed : " + maxSpeed);
+	}
 }
